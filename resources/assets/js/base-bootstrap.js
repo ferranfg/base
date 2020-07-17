0f0894e47b08f6c486c02e3d3da7f73906528279
Ferran Figueredo <ferranfg@gmail.com>
2020-07-17 14:27:23 +0200

    Add slash on URL translate

--- resources/assets/js/base-bootstrap.js
+++ resources/assets/js/base-bootstrap.js
@@ -1,4 +1,4 @@
 Vue.filter('translate', function (value, path) {
     let translation = value[Spark.locale];
-    return path == undefined ? translation : `${path}/${translation}`;
+    return path == undefined ? translation : `/${path}/${translation}`;
 });
