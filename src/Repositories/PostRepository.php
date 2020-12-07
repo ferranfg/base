6306ff9184269c31cc5b03b4570e23833dab01ce
Ferran Figueredo <ferranfg@gmail.com>
2020-12-07 16:40:09 +0100

    Query status on next previous post

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -18,20 +18,22 @@ class PostRepository
         return Base::post()->where("slug->{$locale}", $slug)->firstOrFail();
     }
 
-    public function previousPost($post)
+    public function previousPost($post, $status = 'published')
     {
         $previous_id = Base::post()
             ->whereType($post->type)
+            ->whereStatus($status)
             ->where(Base::post()->getKeyName(), '<', $post->id)
             ->max(Base::post()->getKeyName());
 
         return Base::post()->find($previous_id);
     }
 
-    public function nextPost($post)
+    public function nextPost($post, $status = 'published')
     {
         $next_id = Base::post()
             ->whereType($post->type)
+            ->whereStatus($status)
             ->where(Base::post()->getKeyName(), '>', $post->id)
             ->min(Base::post()->getKeyName());
 
