bdfd922a2bb3d7213c6db1604d645b8c4ab05d4c
Ferran Figueredo <ferranfg@gmail.com>
2021-01-27 19:09:54 +0100

    Add cookie-consent styles

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -78,7 +78,7 @@
         </a>
     </div>
 
-    <?php /* @include('cookieConsent::index') */ ?>
+    @include('cookieConsent::index')
 
     <script src="https://kit.fontawesome.com/ba5226906a.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
