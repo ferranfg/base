94a9ad4f6cc32664c6719dbd803dd0b373d7978d
Ferran Figueredo <ferranfg@gmail.com>
2021-04-15 08:39:32 +0200

    Añadir alt en img de testimonial

--- resources/views/components/testimonial.blade.php
+++ resources/views/components/testimonial.blade.php
@@ -1,5 +1,5 @@
 <div class="media customer-testi m-2">
-    <img src="{{ $review->author_photo_url }}" class="avatar avatar-small mr-3 rounded-circle shadow">
+    <img src="{{ $review->author_photo_url }}" class="avatar avatar-small mr-3 rounded-circle shadow" alt="{{ $review->author_name }}">
     <div class="media-body content p-3 shadow rounded bg-white position-relative">
         <ul class="list-unstyled mb-0">
             @for ($i = 1; $i <= 5; $i++)
