52674e75f0e1c95870d978db7c1391bc28b728a1
Ferran Figueredo <ferranfg@gmail.com>
2021-05-17 07:20:36 +0200

    Update tracking script

--- resources/views/components/tracking.blade.php
+++ resources/views/components/tracking.blade.php
@@ -1,3 +1,3 @@
 @if (app()->isProduction())
-<script async defer data-domain="{{ $tracking_id }}" src="https://analytics.ferranfigueredo.com/js/index.js"></script>
+<script async defer data-domain="{{ $tracking_id }}" src="https://plausible.ferranfigueredo.com/js/index.js"></script>
 @endif
