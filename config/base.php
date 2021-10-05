06ad0a04e0f39d5bf007bf961e47508e797de0cd
Ferran Figueredo <ferranfg@gmail.com>
2021-10-05 14:43:01 +0200

    Update config.base

--- config/base.php
+++ config/base.php
@@ -4,7 +4,38 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Incoming Slack Webhooks 
+    | Meta Information
+    |--------------------------------------------------------------------------
+    |
+    | Información que se incluirá en las cabeceras de la plantilla si se usa conjuntamente
+    | el componente de blade @include('base::components.meta). Incluye información sobre
+    | Open Graph y Twitter Cards
+    |
+    */
+
+    'meta_title' => env('BASE_META_TITLE'),
+
+    'meta_description' => env('BASE_META_DESCRIPTION'),
+
+    'meta_image' => env('BASE_META_IMAGE'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Footer Information
+    |--------------------------------------------------------------------------
+    |
+    | Información que aparecerá en el footer de la web (descripción un poco más larga del site)
+    | y los enlaces a las redes sociales y mail de contacto
+    |
+    */
+
+    'description' => env('BASE_DESCRIPTION'),
+
+    'twitter_username' => env('BASE_TWITTER_USERNAME'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Incoming Slack Webhooks
     |--------------------------------------------------------------------------
     |
     | Incoming webhooks are a simple way to post messages from external sources into Slack.
---
+++ resources/views/components/meta.blade.php
@@ -0,0 +1,22 @@
+<link rel="canonical" href="{{ $meta_url }}">
+
+<meta name="description" content="{{ $meta_description }}">
+
+<meta property="og:type" content="article" />
+<meta property="og:title" content="{{ $meta_title }}" />
+<meta property="og:description" content="{{ $meta_description }}" />
+<meta property="og:url" content="{{ $meta_url }}" />
+<meta property="og:image" content="{{ $meta_image }}" />
+
+<meta property="fb:app_id" content="{{ config('services.facebook.client_id') }}" />
+
+<meta name="theme-color" content="#2f55d4" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="{{ $meta_title }}" />
+<meta name="twitter:description" content="{{ $meta_description }}" />
+<meta name="twitter:url" content="{{ $meta_url }}" />
+<meta name="twitter:image" content="{{ $meta_image }}" />
+<meta name="twitter:site" content="{{ config('pistol.twitter_username') }}" />
+<meta property="og:image:width" content="{{ $og_width }}" />
+<meta property="og:image:height" content="{{ $og_height }}" />
