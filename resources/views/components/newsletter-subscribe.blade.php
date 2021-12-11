32815287e729bdd0a88d7cc733b26cba55b2bbd6
Ferran Figueredo <ferranfg@gmail.com>
2021-12-11 19:05:50 +0100

    Update newsletter component template

--- resources/views/components/newsletter-subscribe.blade.php
+++ resources/views/components/newsletter-subscribe.blade.php
@@ -1,34 +1,35 @@
 <newsletter-subscribe inline-template>
-    <div class="subscribe-newsletter">
-        <h5 class="text-light footer-head">Newsletter</h5>
-        <p class="mt-4">Sign up and receive the latest tips via email.</p>
-        <form>
+    <section class="section bg-primary lazy" data-bg="url('/images/saas/home-shape.png')">
+        <div class="bg-overlay"></div>
+        <div class="container position-relative">
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="foot-subscribe form-group">
-                        <label>Write your email <span class="text-danger">*</span></label>
-                        <div class="position-relative" :class="{'is-invalid': subscribeForm.errors.has('email')}">
-                            <i class="fa fa-envelope icon-sm icons"></i>
-                            <input type="text" class="form-control pl-5 rounded" v-model="subscribeForm.email" placeholder="{{__('Your Email Address')}}" :disabled="subscribeForm.busy">
-                        </div>
-                        <span class="invalid-feedback" v-show="subscribeForm.errors.has('email')">
-                            @{{ subscribeForm.errors.get('email') }}
-                        </span>
-                        <div class="custom-control custom-checkbox mt-2 mb-0">
-                            <input class="custom-control-input" :class="{'is-invalid': subscribeForm.errors.has('terms')}" type="checkbox" value="checked" id="terms" v-model="subscribeForm.terms" :disabled="subscribeForm.busy">
-                            <label class="custom-control-label font-weight-normal" for="terms">I understand and agree to the <a href="/terms" target="_blank">Terms of Service</a> and <a href="/privacy" target="_blank">Privacy Policy</a>.</label>
-                            <span class="invalid-feedback" v-show="subscribeForm.errors.has('terms')">
-                                @{{ subscribeForm.errors.get('terms') }}
+                <div class="col-md-7 mt-4 pt-2 mt-sm-0 pt-sm-0">
+                    <div class="section-title text-center text-md-left">
+                        <h5 class="title text-white title-dark mb-2">{{__('Subscribe to our newsletter')}}</h5>
+                        <p class="text-light para-dark">{{__('Sign up and receive the latest tips via email.')}}</p>
+                    </div>
+                    <div class="subcribe-form mt-4 pt-2">
+                        <form class="m-0">
+                            <div class="form-group mb-0" :class="{'is-invalid': subscribeForm.errors.has('email')}">
+                                <input type="email" name="email" class="rounded-pill" v-model="subscribeForm.email" placeholder="{{__('Your Email Address')}}" :disabled="subscribeForm.busy">
+                                <button type="button" class="btn btn-pills btn-primary" @click.prevent="submit" :disabled="subscribeForm.busy">
+                                    {{__('Subscribe')}}
+                                </button>
+                            </div>
+                            <span class="invalid-feedback" v-show="subscribeForm.errors.has('email')">
+                                @{{ subscribeForm.errors.get('email') }}
                             </span>
-                        </div>
+                            <div class="custom-control custom-checkbox mt-2 mb-0 ml-3 text-white">
+                                <input class="custom-control-input" :class="{'is-invalid': subscribeForm.errors.has('terms')}" type="checkbox" value="checked" id="terms" v-model="subscribeForm.terms" :disabled="subscribeForm.busy">
+                                <label class="custom-control-label small" for="terms">I understand and agree to the <a href="/terms" target="_blank">Terms of Service</a> and <a href="/privacy" target="_blank">Privacy Policy</a>.</label>
+                                <span class="invalid-feedback" v-show="subscribeForm.errors.has('terms')">
+                                    @{{ subscribeForm.errors.get('terms') }}
+                                </span>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="col-lg-12">
-                    <button type="button" class="btn btn-soft-primary btn-block" @click.prevent="submit" :disabled="subscribeForm.busy">
-                        {{__('Subscribe')}}
-                    </button>
-                </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </section>
 </newsletter-subscribe>
