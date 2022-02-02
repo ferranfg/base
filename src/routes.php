03eec599e9bdecb387b8026c5feef3f861ccd0f4
Ferran Figueredo <ferranfg@gmail.com>
2022-02-02 14:47:52 +0100

    404 if feed URL is not available

--- src/routes.php
+++ src/routes.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::feeds('feed');
+if (config('feed.feeds.main.items')) Route::feeds('feed');
 
 Route::group(['middleware' => 'web'], function ()
 {
