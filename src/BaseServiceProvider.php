de5fcea224acc9e92358d4b5c49e956e16a7b84c
Ferran Figueredo <ferranfg@gmail.com>
2022-04-16 16:13:24 +0200

    Update Spark 12 + Cashier 13

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -40,6 +40,7 @@ class BaseServiceProvider extends ServiceProvider
                     __DIR__ . '/../database/migrations/create_events_table.php.stub' => database_path("migrations/{$time}_create_events_table.php"),
                     __DIR__ . '/../database/migrations/create_metadata_table.php.stub' => database_path("migrations/{$time}_create_metadata_table.php"),
                     __DIR__ . '/../database/migrations/update_users_table.php.stub' => database_path("migrations/{$time}_update_users_table.php"),
+                    __DIR__ . '/../database/migrations/update_cashier_13.php.stub' => database_path("migrations/{$time}_update_cashier_13.php"),
                 ], 'migrations');
             }
 
