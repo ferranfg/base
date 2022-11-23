1a9f670e48de52a0c394983757436143ddf7bd8f
Ferran Figueredo <ferranfg@gmail.com>
2022-11-23 16:52:47 +0100

    Provide script variables

--- src/BaseFacade.php
+++ src/BaseFacade.php
@@ -3,6 +3,7 @@
 namespace Ferranfg\Base;
 
 use Illuminate\Support\Facades\Facade;
+use Laravel\Cashier\Cashier;
 
 /**
  * @see \Ferranfg\Base\Base
@@ -24,13 +25,36 @@ class BaseFacade extends Facade
         $user = auth()->user();
 
         return [
+            'translations' => static::getTranslations(),
+            'csrfToken' => csrf_token(),
+            'currency' => config('cashier.currency'),
+            'currencyLocale' => config('cashier.currency_locale'),
+            'env' => config('app.env'),
             'state' => [
                 'user' => $user,
                 'teams' => auth()->check() ? $user->teams : [],
                 'currentTeam' => auth()->check() ? $user->currentTeam : null,
             ],
+            'stripeApiVersion' => Cashier::STRIPE_VERSION,
+            'stripeKey' => config('cashier.key'),
+            'cashierPath' => config('cashier.path'),
             'locale' => config('app.locale')
         ];
     }
 
+    /**
+     * Get the translation keys from file.
+     *
+     * @return array
+     */
+    private static function getTranslations()
+    {
+        $translationFile = base_path('lang/'.app()->getLocale().'.json');
+
+        if (! is_readable($translationFile)) {
+            $translationFile = base_path('lang/'.app('translator')->getFallback().'.json');
+        }
+
+        return json_decode(file_get_contents($translationFile), true);
+    }
 }
