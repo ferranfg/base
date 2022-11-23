efcf77320d14eadfb6ff0678ab69d2d088267d21
Ferran Figueredo <ferranfg@gmail.com>
2022-11-23 17:59:55 +0100

    Fix terms page

--- resources/views/legal/placeholder.blade.php
+++ resources/views/legal/placeholder.blade.php
@@ -1,17 +1,12 @@
-@extends('spark::layouts.app')
+@extends('base::layouts.web')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <div class="card card-default">
-            <div class="card-header"><a href="/">{{ config('app.name') }}</a> &raquo; {{ $title }}</div>
-                <div class="card-body">
-                    <h1 class="mt-2 mb-4">{{ $title }}</h1>
-                    {!! $content !!}
-                </div>
+@section('main')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h1 class="mt-2 mb-4">{{ $title }}</h1>
+                {!! $content !!}
             </div>
         </div>
     </div>
-</div>
 @endsection
