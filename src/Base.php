c5ddb8480cb056fef01379ccb14b91f18c6b3e32
Ferran Figueredo <ferranfg@gmail.com>
2023-01-18 08:08:03 +0100

    Add developers config

--- src/Base.php
+++ src/Base.php
@@ -9,6 +9,15 @@ class Base
     use ManagesModelOptions;
 
     /**
+     * All of the application developer e-mail addresses.
+     *
+     * @var array
+     */
+    public static $developers = [
+        //
+    ];
+
+    /**
      * The user model class name.
      *
      * @var string
@@ -21,4 +30,25 @@ class Base
      * @var string
      */
     public static $teamModel = 'Ferranfg\Base\Models\Team';
+
+    /**
+     * Set the user model class name.
+     *
+     * @param  string  $userModel
+     * @return void
+     */
+    public static function useUserModel($userModel)
+    {
+        static::$userModel = $userModel;
+    }
+
+    /**
+     * Get a new user model instance.
+     *
+     * @return \Ferranfg\Base\Models\User
+     */
+    public static function user()
+    {
+        return new static::$userModel;
+    }
 }
