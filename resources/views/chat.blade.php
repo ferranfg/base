b19603eb8ef3bc03bc83500a6005ac3686931d6c
ferran figueredo <ferranfg@gmail.com>
2023-05-24 07:39:15 +0200

    Añadir assistance a ferranfg/base

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+
+@section('content')
+
+    <section class="bg-half d-table w-100" style="background-image:url({{ $header }});padding:36px">
+        <div class="bg-overlay"></div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="footer p-3 rounded mb-5">
+                        @include('base::components.chat-form')
+                    </div>
+                    @if (config('base.assistance_docs_view'))
+                        <div class="post">
+                            @include(config('base.assistance_docs_view'))
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
