bf5f7ffc7a5e7f8920af14a8228838bc0520b602
Ferran Figueredo <ferranfg@gmail.com>
2023-07-20 16:52:58 +0200

    Add command SendPostNewsletter

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -11,6 +11,7 @@ use Spatie\NovaTranslatable\Translatable;
 use Ferranfg\Base\Commands\PublishCommand;
 use Ferranfg\Base\Commands\InstallCommand;
 use Ferranfg\Base\Commands\EmbeddingsCommand;
+use Ferranfg\Base\Commands\SendPostNewsletter;
 
 class BaseServiceProvider extends ServiceProvider
 {
@@ -49,6 +50,7 @@ class BaseServiceProvider extends ServiceProvider
                 EmbeddingsCommand::class,
                 InstallCommand::class,
                 PublishCommand::class,
+                SendPostNewsletter::class,
             ]);
         }
 
---
+++ src/Commands/SendPostNewsletter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ferranfg\Base\Commands;
+
+use Ferranfg\Base\Models\Post;
+use Illuminate\Console\Command;
+
+class SendPostNewsletter extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'base:send-post-newsletter {postId} {--type=test}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends the current post newsletter';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $post = Post::find($this->argument('postId'));
+
+        if (is_null($post)) return Command::FAILURE;
+
+        $post->sendNewsletter($this->option('type'));
+
+        return Command::SUCCESS;
+    }
+}
