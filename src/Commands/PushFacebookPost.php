ea917b5f51e7525a397f933205e106d02ae707b4
Ferran Figueredo <ferranfg@gmail.com>
2024-01-17 10:34:02 +0100

    Add Push Facebook Post command

---
+++ src/Commands/PushFacebookPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ferranfg\Base\Commands;
+
+use Ferranfg\Base\Base;
+use Illuminate\Console\Command;
+
+class PushFacebookPost extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'base:push-facebook-post';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish on Facebook a random post from the site.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $post = Base::post()->whereStatus('published')->inRandomOrder()->first();
+        $post->publishFacebook();
+
+        return Command::SUCCESS;
+    }
+}
