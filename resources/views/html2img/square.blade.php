1f91b3c60df9025bba451ddf03ddd64975317e3f
Ferran Figueredo <ferranfg@gmail.com>
2024-02-16 08:51:09 +0100

    Move html2img overlay

--- resources/views/html2img/square.blade.php
+++ resources/views/html2img/square.blade.php
@@ -1,3 +1,4 @@
+<div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
 <div class="relative flex flex-col h-full place-content-center text-center text-white">
     <div class="text-xl mb-2">{{ $pre_title }}</div>
     <div class="text-5xl font-black mb-4">{{ $title }}</div>
